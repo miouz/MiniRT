cba59f34a26545006876f4683d546529026d59d3
Mi Zhou <mzhou@student.42.fr>
2025-09-22 15:02:20 +0200

    refactor(minirt.h): refactor minirt.h with norminette

--- minirt.h
+++ minirt.h
@@ -6,88 +6,143 @@
 /*   By: anony <anony@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:37:40 by anony             #+#    #+#             */
-/*   Updated: 2025/09/16 15:15:54 by anony            ###   ########.fr       */
+/*   Updated: 2025/09/19 16:33:24 by anony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# include "intersections/intersection.h"
-# include "vector_operations/vector_operations.h"
 # include <math.h>
 # include <stdio.h>
+# include <stdlib.h>
 
 # define HEIGHT 400
 # define LENGHT 800
 
-// id varie de 1 au nombre de pixel max, la numerotation se fait ligne par ligne
-
-typedef struct s_pixel
+/*==================================MINILIBX_DATA===========================*/
+typedef struct s_minilibx_data
 {
-    int     id;
-    double  x;
-    double  y;
-    double  z;
-}   t_pixel;
+	void	*mlx_p;
+	void	*win_p;
+	void	*img_p;
+	char	*dat;
+	int		bpp;
+	int		line;
+	int		end;
+}	t_minilibx_data;
 
-// type = 1 pour un point et 0 pour un vecteur
-
+/*==================================VECTOR_STRUCTS===========================*/
 typedef struct s_coordinates
 {
-    double  x;
-    double  y;
-    double  z;
-    int     type;
-}   t_coordinates;
+	double	x;
+	double	y;
+	double	z;
+	int		type;
+}	t_coordinates;
 
-typedef struct s_camera
-{
-    t_coordinates   *camera;
-    t_coordinates   *cam_direction;
-    int             fov;
-}   t_camera;
-
-// u et v vecteurs directeurs du plan contenant l ecran. u est orthogonal a l axe y vertical du repere orthonorme de la scene.
-
-typedef struct s_screen
-{
-    t_coordinates   *center;
-    t_coordinates   *u;
-    t_coordinates   *v;
-}   t_screen;
+/*==================================OBJECT_DATA===========================*/
 
 typedef struct s_color
 {
-    int red;
-    int green;
-    int blue;
-}   t_color;
+	int	red;
+	int	green;
+	int	blue;
+}	t_color;
 
 typedef enum e_type
 {
-    SPHERE,
-    PLANE,
-    CYLINDER
-}   t_type;
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_type;
+
+typedef struct s_plane
+{
+	t_coordinates	point;
+	t_coordinates	orhto_vector;
+	t_color			color;
+}	t_plane;
+
+typedef struct s_sphere
+{
+	t_coordinates	center;
+	double			diameter;
+	t_color			color;
+}	t_sphere;
+
+typedef struct s_cylinder
+{
+	t_coordinates	center;
+	t_coordinates	axis_vector;
+	double			diameter;
+	double			height;
+	t_color			color;
+}	t_cylinder;
+
+typedef union u_object_union
+{
+	t_plane		plane;
+	t_sphere	sphere;
+	t_cylinder	cylinder;
+}	t_object_union;
 
 typedef struct s_object
 {
-    t_type type;
-    t_coordinates   center;
-    double  diameter;
-    t_color color;
-    t_coordinates   ortho_vector;
-    t_coordinates   dir_vector;
-    double  height;
-}   t_object;
+	t_type			type;
+	t_object_union	data;
+}	t_object;
 
-// typedef struct s_intersect
-// {
-//     double  x;
-//     double  y;
-//     double  z;
-//     t_object    obj;
-// }   t_intersect;
+typedef struct s_intersect
+{
+	t_coordinates	point;
+	t_object		*obj;
+}	t_intersect;
 
+/*==================================SCENE_INFOS===========================*/
+
+typedef struct s_camera
+{
+	t_coordinates	center;
+	t_coordinates	direction;
+	int				fov;
+}	t_camera;
+
+typedef struct s_ray
+{
+	t_coordinates	origin;
+	t_coordinates	direction;
+}	t_ray;
+
+typedef struct s_ambient_lighting
+{
+	double	intensity;
+	t_color	color;      // au cas où mais pas utile dans la mandatory part
+}	t_ambient_lighting;
+
+typedef struct s_light
+{
+	double			intensity;
+	t_coordinates	source;
+	t_color			color;      // au cas où mais pas utile dans la mandatory part
+}	t_light;
+
+// u et v vecteurs directeurs du plan contenant l ecran. u est orthogonal a l axe y vertical du repere orthonorme de la scene.
+typedef struct s_screen
+{
+	t_coordinates	center;
+	t_coordinates	u;
+	t_coordinates	v;
+}	t_screen;
+
+typedef struct s_data
+{
+	t_camera			camera;
+	t_ambient_lighting	ambient_lighting;
+	t_light				light;
+	t_object			*objects;
+	t_coordinates		pixels[HEIGHT * LENGHT];
+	t_intersect			intersects[HEIGHT * LENGHT];
+	char				*colors;
+}	t_data;
 #endif
