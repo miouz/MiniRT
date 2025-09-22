4bc402e401753c72f83780909816eced5139f854
miouz <mizhouis@gmail.com>
2025-09-22 13:44:06 +0200

    feature(test): add criterion test.c file

---
+++ vector_operations/test_vector.c
@@ -0,0 +1,10 @@
+#include <criterion/criterion.h>
+#include "vector_operations.h"
+#include <stdio.h>
+
+void	vector_print(t_coordinates vector)
+{
+	printf("%f\n%f\n%d\n", vector.x, vector.y, vector.z, vector.type);	
+}
+
+Test()
