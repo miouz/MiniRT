72bcc718a6016aa8b8809898167dfe0693db7bce
Zhou Mi <mizhouis@gmail.com>
2025-09-29 10:26:29 +0200

    update(vector_operation.h): update header

--- vector_operations/vector_operations.h
+++ vector_operations/vector_operations.h
@@ -12,19 +12,22 @@
 
 #ifndef VECTOR_OPERATIONS_H
 # define VECTOR_OPERATIONS_H
-# define EPLISON 0.000001
+# define EPSILON 0.000001
 
 /*==============================ERROR_MSG===================================*/
 # define ERROR_VEC_TYPE "Error: vector conversion type error\n"
+# define ERROR_DIVISION_ZERO "Error: division by zero\n"
 
 /*==============================VECTO_OPERATIONS===========================*/
 t_coordinates	vec_substract(t_coordinates a, t_coordinates b);
 t_coordinates	vec_add(t_coordinates a, t_coordinates b);
-t_coordinates	vec_sclala_multiply(t_coordinates a, double b);
+t_coordinates	vec_scala_multiply(t_coordinates a, double b);
 t_coordinates	vec_scala_divide(t_coordinates a, double b);
 t_coordinates	vec_normalize(t_coordinates a);
-double	vec_dot_product(t_coordinates a, t_coordinates b);
-double	vec_magnitude(t_coordinates a);
+double			vec_dot_product(t_coordinates a, t_coordinates b);
+double			vec_magnitude(t_coordinates a);
+int				vec_equals(t_coordinates a, t_coordinates b);
+t_coordinates	vec_creat(double x, double y, double z, int type);
 
 #endif /* ifdef VECTOR_OPERATIONS_H
  */
