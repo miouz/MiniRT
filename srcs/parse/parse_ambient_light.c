e318f98b0c35f4a9ba757ab516aaddfd2794a121
Zhou Mi <mizhouis@gmail.com>
2025-10-08 11:24:46 +0200

    update(parse_ambient_light): update parameter from *rest to **rest

--- srcs/parse/parse_ambient_light.c
+++ srcs/parse/parse_ambient_light.c
@@ -3,10 +3,10 @@
 int	parse_ambient_light(char *rest, t_data *data)
 {
 	if (get_next_double_in_range(
-			rest, &data->ambient_lighting.intensity, 0, 1) == EXIT_FAILURE)
+			*rest, &data->ambient_lighting.intensity, 0, 1) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (get_next_color(
-			rest, &data->ambient_lighting.color) == EXIT_FAILURE)
+			*rest, &data->ambient_lighting.color) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (no_info_in_rest_of_line(rest) == false)
 		return (EXIT_FAILURE);
