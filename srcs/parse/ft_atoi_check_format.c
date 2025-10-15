2edc034d2a005d5243e07bb6714ef028f8d519aa
Zhou Mi <mizhouis@gmail.com>
2025-10-15 17:27:47 +0200

    feature(ft_atoi_check_format): add ft_atoi_check_format func

--- srcs/parse/ft_atoi_check_format.c
+++ srcs/parse/ft_atoi_check_format.c
@@ -1,15 +1,44 @@
 // #include "../includes/minirt.h"
 #include "parse.h"
 
+/**
+ * @brief swap end with \0 in order to use atoi
+ *
+ * @param result address of int to write value
+ * @param str address of string's begin
+ * @param end address of string's end
+ * @return return result of ft_atoi_check_format
+ */
 int	swap_nul_and_atoi(int *result, char *str, char *end)
 {
 	char	nul;
+	int		res_atoi;
 
 	nul = '\0';
 	ft_swap(end, &nul);
-	*result = ft_atoi(str);
+	res_atoi = ft_atoi_check_format(str, result);
 	ft_swap(end, &nul);
 	return (EXIT_SUCCESS);
+}
+
+/**
+ * @brief atoi and check integer overflow
+ *
+ * @param str address of string
+ * @param result address of integer to write value
+ * @return EXIT_SUCCESS if good format and EXIT_FAILURE if bad format
+ */
+int	ft_atoi_check_format(char *str, int *result )
+{
+	char	*return_itoa;
+
+	*result = ft_atoi(str);
+	return_itoa = ft_itoa(*result);
+	if (return_itoa == NULL)
+		return (EXIT_FAILURE);
+	if (ft_strncmp(str, return_itoa, ft_strlen(str)) == 0)
+		return (free(return_itoa), EXIT_SUCCESS);
+	return (free(return_itoa), EXIT_FAILURE);
 }
 
 int	ft_atoi(const char *nptr)
