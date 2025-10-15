e48febb8d908ccfc0ec649afce742cbec94570ce
Zhou Mi <mizhouis@gmail.com>
2025-10-15 17:27:12 +0200

    update(checkers): update .c

--- srcs/parse/checkers.c
+++ srcs/parse/checkers.c
@@ -1,30 +1,44 @@
 // #include "../includes/minirt.h"
 #include "parse.h"
 
-bool	is_good_color_format(char *line)
-{
-	int	colors_count;
-	int	digit_count;
+// bool	is_good_color_format(char *line)
+// {
+// 	int	colors_count;
+// 	int	digit_count;
+//
+// 	colors_count = 0;
+// 	digit_count = 0;
+// 	while (*line && ft_isdigit(*line) == true)
+// 	{
+// 		digit_count++;
+// 		line++;
+// 		if ((*line == ',' && colors_count <= 1) || (colors_count == 2
+// 				&& (*line == '\0' || is_white_space(*line) == true)))
+// 		{
+// 			if (digit_count <= 3 && digit_count >= 1)
+// 			{
+// 				digit_count = 0;
+// 				colors_count++;
+// 			}
+// 			if (*line)
+// 				line++;
+// 		}
+// 	}
+// 	if (colors_count == 3)
+// 		return (true);
+// 	return (false);
+// }
 
-	colors_count = 0;
-	digit_count = 0;
-	while (*line && ft_isdigit(*line) == true)
-	{
-		digit_count++;
-		line++;
-		if ((*line == ',' && colors_count <= 1) || (colors_count == 2
-				&& (*line == '\0' || is_white_space(*line) == true)))
-		{
-			if (digit_count <= 3 && digit_count >= 1)
-			{
-				digit_count = 0;
-				colors_count++;
-			}
-			if (*line)
-				line++;
-		}
-	}
-	if (colors_count == 3)
+bool	int_is_in_range(int value, int min, int max)
+{
+	if (value >= min && value <= max)
+		return (true);
+	return (false);
+}
+
+bool	double_is_in_range(double value, double min, double max)
+{
+	if (value >= min && value <= max)
 		return (true);
 	return (false);
 }
