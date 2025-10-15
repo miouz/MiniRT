ca76d11155ca15e7a52b7a7e62c56670b6922c51
Zhou Mi <mizhouis@gmail.com>
2025-10-15 17:26:23 +0200

    feature(getters_int) add .c

---
+++ srcs/parse/getters_int.c
@@ -0,0 +1,55 @@
+#include "parse.h"
+
+int	forward_str_get_int(char **line, char **begin)
+{
+	*begin = *line;
+	if (**line == '\0')
+		return (EXIT_FAILURE);
+	if (**line == '-')
+		(*line)++;
+	if (ft_isdigit(**line) == false)
+		return (EXIT_FAILURE);
+	while (ft_isdigit(**line) == true)
+		(*line)++;
+	return (EXIT_SUCCESS);
+}
+
+int	get_next_int_in_range(char **line, int *result, int min, int max)
+{
+	char *begin;
+
+	if (check_and_jump_spaces(line) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (forward_str_get_int(line, &begin) == EXIT_SUCCESS
+		&& swap_nul_and_atoi(result, begin, *line) == EXIT_SUCCESS
+		&& int_is_in_range(*result, min, max) == true)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	get_next_color(char **line, t_color *color)
+{
+	char	*begin;
+	int		result[3];
+	int		index;
+
+	index = 0;
+	if (check_and_jump_spaces(line) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	while (index <= 2
+		&& forward_str_get_int(line, &begin) == EXIT_SUCCESS
+		&& swap_nul_and_atoi(&result[index], begin, *line) == EXIT_SUCCESS
+		&& int_is_in_range(result[index], 0, 255) == true)
+	{
+		if (index <= 1 && **line == ',')
+		{
+			index++;
+			(*line)++;
+		}
+		else if (index == 2 && (**line == '\0'
+				|| is_white_space(**line) == true))
+			return (*color = (t_color){result[0], result[1], result[2]},
+			EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
