062cfd02b51163012b5cd12499956da4f3515104
Zhou Mi <mizhouis@gmail.com>
2025-10-16 22:23:06 +0200

    refactor(parse.h, setup_data): refactor parse.h setup_data
    refactor(setup_data): refactor

--- srcs/parse/parse.h
+++ srcs/parse/parse.h
@@ -13,8 +13,10 @@
 #ifndef PARSE_H
 # define PARSE_H
 
+/*==============================ERROR_MSG===================================*/
 # define ERROR_ARG_NUM "Error: please run ./miniRT <file's name>\n"
 # define ERROR_ARG_FORMAT "Error: file's name should terminate with .rt\n"
+# define ERROR_SCENE_FILE "Error: fail to generate scene with file\n"
 # define ERROR_DOUBLE_FORMAT "Error: need less than 9(int) + 5(frac) digits\n"
 # define ERROR_AML "Error: ambient light not correct\n"
 # define ERROR_CAM "Error: camera not correct\n"
@@ -22,14 +24,19 @@
 # define ERROR_SPHERE "Error: sphere not correct\n"
 # define ERROR_PLANE "Error: plane not correct\n"
 # define ERROR_CYLINDER "Error: cylinder not correct\n"
+# define ERROR_NO_GEOMETRY "Error: no geometry in scene\n"
+# define ERROR_NO_3_OBJ "Error: need 1 ambient light, 1 light and 1 camera\n"
 
+/*==================================NUMBERS===========================*/
 # define MAX_DOUBLE_INT_COUNT 9
 # define MAX_DOUBLE_FRAC_COUNT 5
 # define MAX_DOUBLE_INT 2000000
 # define MAX_DOUBLE_FRAC 4000000
 # define MAX_DOUBLE 9999999
 
-# define ERROR_SCENE_FILE "Error: fail to generate scene with file\n"
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <fcntl.h>
 # include <stdbool.h>
@@ -39,14 +46,31 @@
 # include <math.h>
 # include <limits.h>
 
-/*==================================PARSE_ARG===========================*/
-bool	is_valid_arg(int argc, char **argv);
+//get_next_line_structure
+//bu = buffer, i = index, r = read_size
+typedef struct s_dat
+{
+	char	*bu;
+	size_t	i;
+	size_t	r;
+}	t_dat;
+
+/*==================================PARSE_FUNC===========================*/
+int		check_and_jump_spaces(char **line);
+int		get_line_and_parse(int fd, t_data *data, t_list *obj, int *obj_count);
 int		setup_data(t_data *data, int argc, char **argv);
 
 /*==================================PARSE_OBJECTS===========================*/
 int		parse_ambient_light(char *rest, t_data *data);
 int		parse_camera(char *rest, t_data *data);
 int		parse_light(char *rest, t_data *data);
+
+/*==================================PARSE_GEOMETRY===========================*/
+int		parse_geometry(char *rest,
+			int *parse_func(char *, t_list *), t_list **last_obj, t_data *data);
+int		parse_plane(char *rest, t_object *object);
+int		parse_cylinder(char *rest, t_object *object);
+int		parse_sphere(char *rest, t_object *object);
 
 /*==================================GETTERS===========================*/
 int		get_next_double_in_range(char **str, double *num,
@@ -64,9 +88,11 @@ bool	is_white_space(int c);
 bool	ft_isdigit(int c);
 bool	is_negative_sign(char c);
 bool	is_normalized_vector(t_coordinates vector);
+bool	scene_is_valid(t_data *data, int *obj_count);
+bool	is_valid_arg(int argc, char **argv);
+
 
 /*==================================UTILS===========================*/
-int		check_and_jump_spaces(char **line);
 int		ft_atod_check_format(char *str, double *result);
 int		ft_atoi_check_format(char *str, int *result );
 int		ft_atoi(const char *nptr);
@@ -77,8 +103,9 @@ void	ft_swap(char *a, char *b);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 t_list	*ft_lstnew(void *content);
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void **));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-int	setup_data(t_data *data, int argc, char **argv);
+char	*get_next_line(int fd);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif // !PARSE_H
