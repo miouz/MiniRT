f2593baa95e22d720026b88365aa25d7c05e9e3d
Zhou Mi <mizhouis@gmail.com>
2025-10-16 22:26:01 +0200

    refactor(checkers):split checkers into 2 files
    update(checkers_setup_data): split checkers into 2 files

---
+++ srcs/parse/checkers_setup_data.c
@@ -0,0 +1,26 @@
+#include "../includes/minirt.h"
+
+bool	is_valid_arg(int argc, char **argv)
+{
+	int	size;
+	int	pos_point;
+
+	if (argc != 2)
+		return (error_msg(ERROR_ARG_NUM), false);
+	size = ft_strlen(argv[1]);
+	pos_point = size - 4;
+	if (ft_strncmp(argv[1] + pos_point, ".rt", 4) != 0)
+		return (error_msg(ERROR_ARG_FORMAT), false);
+	return (true);
+}
+
+bool	scene_is_valid(t_data *data, int *obj_count)
+{
+	if (data->nb_objects == 0)
+		return (error_msg(ERROR_NO_GEOMETRY), false);
+	if (obj_count[0] != 1
+		|| obj_count[1] != 1
+		|| obj_count[2] != 1)
+		return (error_msg(ERROR_NO_3_OBJ), false);
+	return (true);
+}
