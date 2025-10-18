b34695f17aac3e3168b8231441217b2f394c1cba
miouz <mizhouis@gmail.com>
2025-10-18 14:27:23 +0200

    fix(setup_data): fix data.t_objects segfault bug
    before the address of pointer is not passed to subfunctions correctly

--- srcs/parse/setup_data.c
+++ srcs/parse/setup_data.c
@@ -30,22 +30,25 @@ static int	init_parse_data(t_data *data, int *obj_count)
 	return (EXIT_SUCCESS);
 }
 
-static int	obj_lst_to_array(t_list *lst, t_object *objects, int count)
+static int	obj_lst_to_array(t_list *lst, t_object **objects, int count)
 {
 	int	index;
 
 	if (!lst)
 		return (EXIT_FAILURE);
-	objects = malloc(count * sizeof(t_object));
-	if (objects == NULL)
+	*objects = malloc(count * sizeof(t_object));
+	if (*objects == NULL)
 		return (perror("malloc"), EXIT_FAILURE);
 	index = 0;
 	while (index < count)
 	{
-		if (ft_memcpy(&objects[index], lst->content, sizeof(t_object)))
+		if (ft_memcpy(&(*objects)[index], lst->content, sizeof(t_object)))
+		{
+			lst = lst->next;
 			index++;
+		}
 		else
-			return (free(objects), objects = NULL, EXIT_FAILURE);
+			return (free(*objects), *objects = NULL, EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
@@ -78,7 +81,8 @@ int	setup_data(t_data *data, int argc, char **argv)
 	}
 	if (scene_is_valid(data, obj_count) == false)
 		exit(EXIT_FAILURE);
-	if (obj_lst_to_array(head, data->objects, data->nb_objects) == EXIT_FAILURE)
+	if (obj_lst_to_array(head, &data->objects, data->nb_objects)
+		== EXIT_FAILURE)
 	{
 		ft_lstclear(&head, free);
 		exit(EXIT_FAILURE);
