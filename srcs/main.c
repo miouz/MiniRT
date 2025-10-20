2830fda8c4b66743a907fcd7ae13009bcbd6382d
Adrien Nony <anony@f6r7s3.clusters.42paris.fr>
2025-10-20 17:19:32 +0200

    norme main.c, changement scenetest3.rt : chat de kevin

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: anony <anony@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 16:38:44 by anony             #+#    #+#             */
-/*   Updated: 2025/10/20 16:40:52 by anony            ###   ########.fr       */
+/*   Updated: 2025/10/20 16:50:36 by anony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,14 +67,14 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc ==1)
+	if (argc == 1)
 	{
-	initialize_minilibx(&data.minilibx);
-	mlx_put_image_to_window(data.minilibx.mlx_p, data.minilibx.win_p,
-		data.minilibx.img_p, 0, 0);
-	mlx_hook(data.minilibx.win_p, 17, 0, &free_data_and_exit_empty, &data);
-	mlx_hook(data.minilibx.win_p, 2, 1L << 0, &keyboard_event, &data);
-	mlx_loop(data.minilibx.mlx_p);
+		initialize_minilibx(&data.minilibx);
+		mlx_put_image_to_window(data.minilibx.mlx_p, data.minilibx.win_p,
+			data.minilibx.img_p, 0, 0);
+		mlx_hook(data.minilibx.win_p, 17, 0, &free_data_and_exit_empty, &data);
+		mlx_hook(data.minilibx.win_p, 2, 1L << 0, &keyboard_event, &data);
+		mlx_loop(data.minilibx.mlx_p);
 	}
 	setup_data(&data, argc, argv);
 	init_pixel_intersects(&data);
